c333efae8b40d37adccda3292bfe0bd084ffa450
arnaud.yanga <arnaud.yanga@believedigital.com>
2020-02-04 17:00:49 +0100

    docker

---
+++ docker/mysql/script/init.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS `dico`;
+ CREATE DATABASE  `dico`;
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+    `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
